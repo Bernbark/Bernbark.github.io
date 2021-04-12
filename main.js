a169905984ce31d073d925991e27eacc26f36707
Bernbark <korystennett@yahoo.com>
2021-04-12 07:48:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -155,14 +155,14 @@ function decreaseCostPerSecond(){
 
 //per click version of the above func
 function decreaseCostPerClick(){
+    if(gameData.dcpCount > 1 && gameData.goldPerClickCost<10000){
+        gameData.goldPerClickCost = 10000
+    }
     if (gameData.gold > gameData.priceDropCost && gameData.goldPerClickCost > 10000){
         gameData.gold -= gameData.priceDropCost
         gameData.dcpCount*=1.5
         gameData.goldPerClickCost-=gameData.dcpCount
         gameData.priceDropCost*=3.3
-        if(gameData.dcpCount > 1 && gameData.goldPerClickCost<10000){
-            gameData.goldPerClickCost = 10000
-        }
         
     }
 }
